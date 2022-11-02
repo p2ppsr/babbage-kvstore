69fb5fff6b557d758ec8afc7cd3269def24af269
Samuel D. Villegas <villegsa@oregonstate.edu>
2022-11-02 16:35:06 -0700

    Add `remove` function

--- kvstore.js
+++ kvstore.js
@@ -78,8 +78,6 @@ const set = async (key, value) => {
 
   if (existing_tokens.length > 0 && existing_tokens[0].value != value) {
     const kvstoreToken = existing_tokens[0]
-    console.log(kvstoreToken)
-    debugger
     const unlockingScript = await pushdrop.redeem({
       prevTxId: kvstoreToken.token.txid,
       outputIndex: kvstoreToken.token.outputIndex,
@@ -118,7 +116,7 @@ const set = async (key, value) => {
     await SDK.createAction({
       description: `Set a value for ${key}`,
       outputs: [{
-        satoshis: 500,
+        satoshis: 1000,
         script: await pushdrop.create({
           fields: [
             KVSTORE_PROTOCOL_ADDRESS,
@@ -134,6 +132,64 @@ const set = async (key, value) => {
   }
 }
 
-// TODO: delete
+/**
+ * Deletes a value from the store.
+ *
+ * @param {String} key The key for the value to set
+ * @param {String} value The value to store
+ *
+ * @returns {Promise} Promise that resolves when the value has been deleted
+ */
+const remove = async (key) => {
+  const protectedKey = await SDK.createHmac({
+    data: Uint8Array.from(Buffer.from(key)),
+    protocolID: [0, 'kvstore'],
+    keyID: key
+  })
 
-module.exports = { get, set }
+  const existing_tokens = await parapet({
+    bridge: KVSTORE_PROTOCOL_ADDRESS,
+    request: {
+      type: 'json-query',
+      query: {
+        v: 3,
+        q: {
+          collection: 'kvstore',
+          find: {
+            protectedKey: Buffer.from(protectedKey).toString('base64')
+          },
+          limit: 1
+        }
+      }
+    },
+    resolvers: ['http://localhost:3103']
+  })
+
+  if (existing_tokens.length > 0) {
+    const kvstoreToken = existing_tokens[0]
+    const unlockingScript = await pushdrop.redeem({
+      prevTxId: kvstoreToken.token.txid,
+      outputIndex: kvstoreToken.token.outputIndex,
+      lockingScript: kvstoreToken.token.lockingScript,
+      outputAmount: kvstoreToken.token.outputAmount,
+      protocolID: [0, 'kvstore'],
+      keyID: key
+    })
+
+    await SDK.createAction({
+      description: `Delete the value for ${key}`,
+      inputs: {
+        [kvstoreToken.token.txid]: {
+          ...kvstoreToken.token,
+          outputsToRedeem: [{
+            index: kvstoreToken.token.outputIndex,
+            unlockingScript
+          }]
+        }
+      },
+      bridges: [KVSTORE_PROTOCOL_ADDRESS]
+    })
+  }
+}
+
+module.exports = { get, set, remove }
